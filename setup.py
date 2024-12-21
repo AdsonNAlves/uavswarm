996dbfe89436fc8a04bc6fbf05d3dd03e3072152
AdsonNAlves <adsonnalves@gmail.com>
2024-12-20 22:47:19 -0300

    deploy: add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='UAV Swarm - Cognitive and Adaptative Approach',
+     dependency_links=[
+     ],
+    version='1.0.0',
+    description='Collaborative UAV Swarm - Cognitive and Adaptative approach using Webots Simulator.',
+    author='Adson Nogueira Alves',
+    license='MIT'
+)
+
